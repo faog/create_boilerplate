2e3c9f8f62fc830849a9326e219d7c1711f124e3
faog <fabiola.orellana.g@gmail.com>
2020-04-16 15:55:55 -0400

    feat: configuración stylelintrc

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './app.css'
 
 const App = () => {
   return (
     <div>
-      <h2>Creación boilerplate sin create-react-app</h2>
+      <h2>Creación boilerplate sin create-react-app</h2> 
       <h4>Autor: Fabiola Orellana</h4>
     </div>
   );
